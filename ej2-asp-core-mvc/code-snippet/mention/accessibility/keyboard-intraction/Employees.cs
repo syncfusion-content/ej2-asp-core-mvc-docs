aab8da9f69acab3f009c7c3067181beaf5c04d14
Kokila13081999 <93525217+Kokila13081999@users.noreply.github.com>
2022-11-01 17:14:33 +0530

    documentation(EJ2-63820): Addressed the review comments

--- ej2-asp-core-mvc/code-snippet/mention/accessibility/keyboard-intraction/Employees.cs
+++ ej2-asp-core-mvc/code-snippet/mention/accessibility/keyboard-intraction/Employees.cs
@@ -8,19 +8,18 @@ namespace WebApplication1.Models
     public class Employees
     {
         public string Name { get; set; }
-        public string Id { get; set; }
+        public string ID { get; set; }
         public List<Employees> GetData()
         {
             List<Employees> data = new List<Employees>
             {
-                new Employees() { Name = "Andrew Fuller", Id = "1" },
-                new Employees() { Name = "Anne Dodsworth", Id = "2" },
-                new Employees() { Name = "Janet Leverling", Id = "3" },
-                new Employees() { Name = "Laura Callahan", Id = "4" },
-                new Employees() { Name = "Margaret Peacock", Id = "5" }
+                new Employees() { Name = "Andrew Fuller", ID = "1" },
+                new Employees() { Name = "Anne Dodsworth", ID = "2" },
+                new Employees() { Name = "Janet Leverling", ID = "3" },
+                new Employees() { Name = "Laura Callahan", ID = "4" },
+                new Employees() { Name = "Margaret Peacock", ID = "5" }
             };
             return data;
         }
     }
 }
-
