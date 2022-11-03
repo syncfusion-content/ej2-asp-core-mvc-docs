5d46eb3e911f3cd5701bf610c1f1243265d67f38
Kokila13081999 <93525217+Kokila13081999@users.noreply.github.com>
2022-11-03 18:28:06 +0530

    documentation(EJ2-65489): changes corrected in hotfix and development

--- ej2-asp-core-mvc/code-snippet/mention/working-with-data/complex-data/Complex.cs
+++ ej2-asp-core-mvc/code-snippet/mention/working-with-data/complex-data/Complex.cs
@@ -14,19 +14,22 @@ namespace WebApplication1.Models
     {
         public string Name { get; set; }
     }
+
     public class Complex
     {
         public Country Country { get; set; }
         public Code Code { get; set; }
         public List<Complex> GetData()
         {
-            List<Complex> data = new List<Complex>();
-            data.Add(new Complex() { Country = new Country() { Name = "Australia" }, Code = new Code() { Id = "AU" } });
-            data.Add(new Complex() { Country = new Country() { Name = "Bermuda" }, Code = new Code() { Id = "BM" } });
-            data.Add(new Complex() { Country = new Country() { Name = "Canada" }, Code = new Code() { Id = "CA" } });
-            data.Add(new Complex() { Country = new Country() { Name = "Cameroon" }, Code = new Code() { Id = "CM" } });
-            data.Add(new Complex() { Country = new Country() { Name = "Denmark" }, Code = new Code() { Id = "DK" } });
-            data.Add(new Complex() { Country = new Country() { Name = "France" }, Code = new Code() { Id = "FR" } });
+            List<Complex> data = new List<Complex>
+            {
+                new Complex() { Country = new Country() { Name = "Australia" }, Code = new Code() { Id = "AU" } },
+                new Complex() { Country = new Country() { Name = "Bermuda" }, Code = new Code() { Id = "BM" } },
+                new Complex() { Country = new Country() { Name = "Canada" }, Code = new Code() { Id = "CA" } },
+                new Complex() { Country = new Country() { Name = "Cameroon" }, Code = new Code() { Id = "CM" } },
+                new Complex() { Country = new Country() { Name = "Denmark" }, Code = new Code() { Id = "DK" } },
+                new Complex() { Country = new Country() { Name = "France" }, Code = new Code() { Id = "FR" } }
+            };
             return data;
         }
     }
