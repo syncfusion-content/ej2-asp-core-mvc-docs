04667903779affd6667397fa9b325219b174cae9
saranya <saranya.dhayalan@syncfusion.com>
2022-11-16 15:26:12 +0530

    UG(EJ2-63628):Changed the code snippets

--- ej2-asp-core-mvc/code-snippet/query-builder/model-binding/default.cs
+++ ej2-asp-core-mvc/code-snippet/query-builder/model-binding/default.cs
@@ -4,7 +4,9 @@ public ActionResult Index()
             ViewBag.values = values;
             var fieldModel = new { allowFiltering = true, popupHeight = "380px" };
             var operatorModel = new { allowFiltering = true, popupHeight = "380px" };
+            var valueModel = new { numericTextBoxModel = new { cssClass = "e-custom" } };
             ViewBag.fieldModel = fieldModel;
             ViewBag.operatorModel = operatorModel;
+            ViewBag.valueModel = valueModel;
             return View();
     }
