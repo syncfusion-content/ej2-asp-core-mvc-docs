17d36faf82cafa1418ac43fa695722a37e65904e
Kokila13081999 <93525217+Kokila13081999@users.noreply.github.com>
2022-12-27 13:24:03 +0530

    documentation(EJ2-65674): UG documentation for Mention integration with Rich Text Editor

--- ej2-asp-core-mvc/code-snippet/rich-text-editor/mention-integration/SportsData.cs
+++ ej2-asp-core-mvc/code-snippet/rich-text-editor/mention-integration/SportsData.cs
@@ -14,28 +14,28 @@ namespace WebApplication1.Models
         public List<SportsData> SportsList()
         {
             List<SportsData> mention = new List<SportsData>();
-            mention.Add(new SportsData { Name = "Selma Rose", EmployeeImage = "3", EmailId = "selma@gmail.com" });
-            mention.Add(new SportsData { Name = "Russo Kay", EmployeeImage = "8", EmailId = "russo@gmail.com" });
-            mention.Add(new SportsData { Name = "Camden Kate", EmployeeImage = "9", EmailId = "camden@gmail.com" });
-            mention.Add(new SportsData { Name = "Mary Kate", EmployeeImage = "4", EmailId = "mary@gmail.com" });
-            mention.Add(new SportsData { Name = "Ursula Ann", EmployeeImage = "2", EmailId = "ursula@gmail.com" });
-            mention.Add(new SportsData { Name = "Margaret", EmployeeImage = "5", EmailId = "margaret@gmail.com" });
-            mention.Add(new SportsData { Name = "Laura Grace", EmployeeImage = "6", EmailId = "laura@gmail.com" });
-            mention.Add(new SportsData { Name = "Robert", EmployeeImage = "8", EmailId = "robert@gmail.com" });
-            mention.Add(new SportsData { Name = "Albert", EmployeeImage = "9", EmailId = "albert@gmail.com" });
-            mention.Add(new SportsData { Name = "Michale", EmployeeImage = "10", EmailId = "michale@gmail.com" });
-            mention.Add(new SportsData { Name = "Andrew James", EmployeeImage = "7", EmailId = "james@gmail.com" });
-            mention.Add(new SportsData { Name = "Rosalie", EmployeeImage = "4", EmailId = "rosalie@gmail.com" });
-            mention.Add(new SportsData { Name = "Stella Ruth", EmployeeImage = "2", EmailId = "stella@gmail.com" });
-            mention.Add(new SportsData { Name = "Richard Rose", EmployeeImage = "10", EmailId = "richard@gmail.com" });
-            mention.Add(new SportsData { Name = "Gabrielle", EmployeeImage = "3", EmailId = "gabrielle@gmail.com" });
-            mention.Add(new SportsData { Name = "Thomas", EmployeeImage = "7", EmailId = "thomas@gmail.com" });
-            mention.Add(new SportsData { Name = "Charles Danny", EmployeeImage = "8", EmailId = "charles@gmail.com" });
-            mention.Add(new SportsData { Name = "Daniel", EmployeeImage = "10", EmailId = "daniel@gmail.com" });
-            mention.Add(new SportsData { Name = "Matthew", EmployeeImage = "7", EmailId = "matthew@gmail.com" });
-            mention.Add(new SportsData { Name = "Donald Krish", EmployeeImage = "9", EmailId = "donald@gmail.com" });
-            mention.Add(new SportsData { Name = "Yohana", EmployeeImage = "1", EmailId = "yohana@gmail.com" });
-            mention.Add(new SportsData { Name = "Kevin Paul", EmployeeImage = "10", EmailId = "kevin@gmail.com" });
+            mention.Add(new SportsData { Name = "Selma Rose", EmployeeImage = "3.png", EmailId = "selma@gmail.com" });
+            mention.Add(new SportsData { Name = "Russo Kay", EmployeeImage = "8.png", EmailId = "russo@gmail.com" });
+            mention.Add(new SportsData { Name = "Camden Kate", EmployeeImage = "9.png", EmailId = "camden@gmail.com" });
+            mention.Add(new SportsData { Name = "Mary Kate", EmployeeImage = "4.png", EmailId = "mary@gmail.com" });
+            mention.Add(new SportsData { Name = "Ursula Ann", EmployeeImage = "2.png", EmailId = "ursula@gmail.com" });
+            mention.Add(new SportsData { Name = "Margaret", EmployeeImage = "5.png", EmailId = "margaret@gmail.com" });
+            mention.Add(new SportsData { Name = "Laura Grace", EmployeeImage = "6.png", EmailId = "laura@gmail.com" });
+            mention.Add(new SportsData { Name = "Robert", EmployeeImage = "8.png", EmailId = "robert@gmail.com" });
+            mention.Add(new SportsData { Name = "Albert", EmployeeImage = "9.png", EmailId = "albert@gmail.com" });
+            mention.Add(new SportsData { Name = "Michale", EmployeeImage = "5.png", EmailId = "michale@gmail.com" });
+            mention.Add(new SportsData { Name = "Andrew James", EmployeeImage = "7.png", EmailId = "james@gmail.com" });
+            mention.Add(new SportsData { Name = "Rosalie", EmployeeImage = "4.png", EmailId = "rosalie@gmail.com" });
+            mention.Add(new SportsData { Name = "Stella Ruth", EmployeeImage = "2.png", EmailId = "stella@gmail.com" });
+            mention.Add(new SportsData { Name = "Richard Rose", EmployeeImage = "8.png", EmailId = "richard@gmail.com" });
+            mention.Add(new SportsData { Name = "Gabrielle", EmployeeImage = "3.png", EmailId = "gabrielle@gmail.com" });
+            mention.Add(new SportsData { Name = "Thomas", EmployeeImage = "7.png", EmailId = "thomas@gmail.com" });
+            mention.Add(new SportsData { Name = "Charles Danny", EmployeeImage = "8.png", EmailId = "charles@gmail.com" });
+            mention.Add(new SportsData { Name = "Daniel", EmployeeImage = "5.png", EmailId = "daniel@gmail.com" });
+            mention.Add(new SportsData { Name = "Matthew", EmployeeImage = "7.png", EmailId = "matthew@gmail.com" });
+            mention.Add(new SportsData { Name = "Donald Krish", EmployeeImage = "9.png", EmailId = "donald@gmail.com" });
+            mention.Add(new SportsData { Name = "Yohana", EmployeeImage = "1.png", EmailId = "yohana@gmail.com" });
+            mention.Add(new SportsData { Name = "Kevin Paul", EmployeeImage = "5.png", EmailId = "kevin@gmail.com" });
             return mention;
         }
 
--- ej2-asp-core-mvc/rich-text-editor/EJ2_ASP.MVC/mention-integration.md
+++ ej2-asp-core-mvc/rich-text-editor/EJ2_ASP.MVC/mention-integration.md
@@ -1,18 +1,18 @@
 ---
 layout: post
-title: Mention Integration in ##Platform_Name## Rich Text Editor Component
-description: Learn here all about Mention Integration in Syncfusion ##Platform_Name## Rich Text Editor component of Syncfusion Essential JS 2 and more.
+title: Mention Integration in ##Platform_Name## Rich Text Editor | Syncfusion
+description: Learn here all about Mention Integration in Syncfusion ##Platform_Name## Rich Text Editor control of Syncfusion Essential JS 2 and more.
 platform: ej2-asp-core-mvc
 control: Mention Integration
 publishingplatform: ##Platform_Name##
 documentation: ug
 ---
 
-# Mention Integration
+# Mention Integration with Rich Text Editor
 
-By integrating the [Mention control](https://ej2.syncfusion.com/aspnetmvc/documentation/mention/getting-started) with a Rich Text Editor, users can easily mention or tag other users or objects from the suggested list without having to manually type out their names or other identifying information.
+By integrating the [Mention](https://ej2.syncfusion.com/aspnetmvc/documentation/mention/getting-started) control with a Rich Text Editor, users can easily mention or tag other users or objects from the suggested list without having to manually type out their names or other identifying information.
 
-The [Target](https://help.syncfusion.com/cr/aspnetmvc-js2/Syncfusion.EJ2.DropDowns.Mention.html#Syncfusion_EJ2_DropDowns_Mention_Target) property of the Mention control allows you to specify the ID of the Rich Text Editor that you want to bind the Mention control to.
+The [Target](https://help.syncfusion.com/cr/aspnetmvc-js2/Syncfusion.EJ2.DropDowns.Mention.html#Syncfusion_EJ2_DropDowns_Mention_Target) property of the Mention control allows you to specify the `ID` of the content editable div element within the Rich Text Editor that you want to bind the Mention control to. This allows you to enable the Mention functionality within the Rich Text Editor, so that users can mention or tag other users or objects from the suggested list while editing the text.
 
 When the user types the `@` symbol followed by a character, the Rich Text Editor will display a list of suggestions for items that the user can select from. The user can then select an item from the list by clicking on it, or by typing the name of the item they want to tag.
 
