6ad63c54c9088a9d6f6d3cd8805a4b769ecccaa2
Kokila13081999 <93525217+Kokila13081999@users.noreply.github.com>
2022-12-28 18:57:01 +0530

    documentation(EJ2-65674): UG documentation for Mention integration with Rich Text Editor

--- ej2-asp-core-mvc/code-snippet/rich-text-editor/mention-integration/SportsData.cs
+++ ej2-asp-core-mvc/code-snippet/rich-text-editor/mention-integration/SportsData.cs
@@ -13,29 +13,32 @@ namespace WebApplication1.Models
 
         public List<SportsData> SportsList()
         {
-            List<SportsData> mention = new List<SportsData>();
-            mention.Add(new SportsData { Name = "Selma Rose", EmployeeImage = "3.png", EmailId = "selma@gmail.com" });
-            mention.Add(new SportsData { Name = "Russo Kay", EmployeeImage = "8.png", EmailId = "russo@gmail.com" });
-            mention.Add(new SportsData { Name = "Camden Kate", EmployeeImage = "9.png", EmailId = "camden@gmail.com" });
-            mention.Add(new SportsData { Name = "Mary Kate", EmployeeImage = "4.png", EmailId = "mary@gmail.com" });
-            mention.Add(new SportsData { Name = "Ursula Ann", EmployeeImage = "2.png", EmailId = "ursula@gmail.com" });
-            mention.Add(new SportsData { Name = "Margaret", EmployeeImage = "5.png", EmailId = "margaret@gmail.com" });
-            mention.Add(new SportsData { Name = "Laura Grace", EmployeeImage = "6.png", EmailId = "laura@gmail.com" });
-            mention.Add(new SportsData { Name = "Robert", EmployeeImage = "8.png", EmailId = "robert@gmail.com" });
-            mention.Add(new SportsData { Name = "Albert", EmployeeImage = "9.png", EmailId = "albert@gmail.com" });
-            mention.Add(new SportsData { Name = "Michale", EmployeeImage = "5.png", EmailId = "michale@gmail.com" });
-            mention.Add(new SportsData { Name = "Andrew James", EmployeeImage = "7.png", EmailId = "james@gmail.com" });
-            mention.Add(new SportsData { Name = "Rosalie", EmployeeImage = "4.png", EmailId = "rosalie@gmail.com" });
-            mention.Add(new SportsData { Name = "Stella Ruth", EmployeeImage = "2.png", EmailId = "stella@gmail.com" });
-            mention.Add(new SportsData { Name = "Richard Rose", EmployeeImage = "8.png", EmailId = "richard@gmail.com" });
-            mention.Add(new SportsData { Name = "Gabrielle", EmployeeImage = "3.png", EmailId = "gabrielle@gmail.com" });
-            mention.Add(new SportsData { Name = "Thomas", EmployeeImage = "7.png", EmailId = "thomas@gmail.com" });
-            mention.Add(new SportsData { Name = "Charles Danny", EmployeeImage = "8.png", EmailId = "charles@gmail.com" });
-            mention.Add(new SportsData { Name = "Daniel", EmployeeImage = "5.png", EmailId = "daniel@gmail.com" });
-            mention.Add(new SportsData { Name = "Matthew", EmployeeImage = "7.png", EmailId = "matthew@gmail.com" });
-            mention.Add(new SportsData { Name = "Donald Krish", EmployeeImage = "9.png", EmailId = "donald@gmail.com" });
-            mention.Add(new SportsData { Name = "Yohana", EmployeeImage = "1.png", EmailId = "yohana@gmail.com" });
-            mention.Add(new SportsData { Name = "Kevin Paul", EmployeeImage = "5.png", EmailId = "kevin@gmail.com" });
+            List<SportsData> mention = new List<SportsData>()
+            {
+                new SportsData { Name = "Selma Rose", EmployeeImage = "3.png", EmailId = "selma@gmail.com" },
+                new SportsData { Name = "Selma Rose", EmployeeImage = "3.png", EmailId = "selma@gmail.com" },
+                new SportsData { Name = "Russo Kay", EmployeeImage = "8.png", EmailId = "russo@gmail.com" },
+                new SportsData { Name = "Camden Kate", EmployeeImage = "9.png", EmailId = "camden@gmail.com" },
+                new SportsData { Name = "Mary Kate", EmployeeImage = "4.png", EmailId = "mary@gmail.com" },
+                new SportsData { Name = "Ursula Ann", EmployeeImage = "2.png", EmailId = "ursula@gmail.com" },
+                new SportsData { Name = "Margaret", EmployeeImage = "5.png", EmailId = "margaret@gmail.com" },
+                new SportsData { Name = "Laura Grace", EmployeeImage = "6.png", EmailId = "laura@gmail.com" },
+                new SportsData { Name = "Robert", EmployeeImage = "8.png", EmailId = "robert@gmail.com" },
+                new SportsData { Name = "Albert", EmployeeImage = "9.png", EmailId = "albert@gmail.com" },
+                new SportsData { Name = "Michale", EmployeeImage = "5.png", EmailId = "michale@gmail.com" },
+                new SportsData { Name = "Andrew James", EmployeeImage = "7.png", EmailId = "james@gmail.com" },
+                new SportsData { Name = "Rosalie", EmployeeImage = "4.png", EmailId = "rosalie@gmail.com" },
+                new SportsData { Name = "Stella Ruth", EmployeeImage = "2.png", EmailId = "stella@gmail.com" },
+                new SportsData { Name = "Richard Rose", EmployeeImage = "8.png", EmailId = "richard@gmail.com" },
+                new SportsData { Name = "Gabrielle", EmployeeImage = "3.png", EmailId = "gabrielle@gmail.com" },
+                new SportsData { Name = "Thomas", EmployeeImage = "7.png", EmailId = "thomas@gmail.com" },
+                new SportsData { Name = "Charles Danny", EmployeeImage = "8.png", EmailId = "charles@gmail.com" },
+                new SportsData { Name = "Daniel", EmployeeImage = "5.png", EmailId = "daniel@gmail.com" },
+                new SportsData { Name = "Matthew", EmployeeImage = "7.png", EmailId = "matthew@gmail.com" },
+                new SportsData { Name = "Donald Krish", EmployeeImage = "9.png", EmailId = "donald@gmail.com" },
+                new SportsData { Name = "Yohana", EmployeeImage = "1.png", EmailId = "yohana@gmail.com" },
+                new SportsData { Name = "Kevin Paul", EmployeeImage = "5.png", EmailId = "kevin@gmail.com" },
+            };
             return mention;
         }
 
