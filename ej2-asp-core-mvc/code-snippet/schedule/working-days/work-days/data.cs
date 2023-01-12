3265009a2905658530140f6c4ee1f2471ae2ef75
Joseph Herson Thomas <josephherson.t@syncfusion.com>
2023-01-12 13:05:58 +0530

    Documentation(BLAZ-26590):Include the screenshots for all the explorer functionalities in asp.net core

--- ej2-asp-core-mvc/code-snippet/schedule/working-days/work-days/data.cs
+++ ej2-asp-core-mvc/code-snippet/schedule/working-days/work-days/data.cs
@@ -9,9 +9,9 @@ public List<AppointmentData> GetScheduleData()
 {
     List<AppointmentData> appData = new List<AppointmentData>();
     appData.Add(new AppointmentData
-    { Id = 1, Subject = "Blue Moon Eclipse", StartTime = new DateTime(2018, 2, 13, 9, 30, 0), EndTime = new DateTime(2018, 2, 13, 11, 0, 0) });
+    { Id = 1, Subject = "Blue Moon Eclipse", StartTime = new DateTime(2023, 2, 13, 9, 30, 0), EndTime = new DateTime(2023, 2, 13, 11, 0, 0) });
     appData.Add(new AppointmentData
-    { Id = 2, Subject = "Milky Way as Melting pot", StartTime = new DateTime(2018, 2, 15, 9, 30, 0), EndTime = new DateTime(2018, 2, 15, 11, 0, 0) });
+    { Id = 2, Subject = "Milky Way as Melting pot", StartTime = new DateTime(2023, 2, 15, 9, 30, 0), EndTime = new DateTime(2023, 2, 15, 11, 0, 0) });
     return appData;
 }
     
