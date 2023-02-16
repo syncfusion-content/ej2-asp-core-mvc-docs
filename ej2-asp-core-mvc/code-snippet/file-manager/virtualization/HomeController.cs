5e1d1a75c02ae4dc3502da825efc9b19a8fb1a49
suba-1997 <subalakshmi.r@syncfusion.com>
2023-02-16 14:34:53 +0530

    documentation(EJ2-68587):ASP MVC core File Manager Virtualization.

---
+++ ej2-asp-core-mvc/code-snippet/file-manager/virtualization/HomeController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
