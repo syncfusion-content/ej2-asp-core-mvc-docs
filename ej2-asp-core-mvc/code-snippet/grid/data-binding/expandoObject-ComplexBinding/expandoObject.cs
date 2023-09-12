e1dfc58803e8492646b3dca1821ff2356d1cfc9c
praveenksf3488 <praveenkumark.sf4388@gmail.com>
2023-09-12 19:20:16 +0530

    documentation(EJ2-847480): Add UG documentation in ASP MVC & Core platform for expando and dynamic Object binding in grid.

--- ej2-asp-core-mvc/code-snippet/grid/data-binding/expandoObject-ComplexBinding/expandoObject.cs
+++ ej2-asp-core-mvc/code-snippet/grid/data-binding/expandoObject-ComplexBinding/expandoObject.cs
@@ -9,7 +9,7 @@ public IActionResult Index()
         d.Customer = new ExpandoObject();
         d.Customer.Freight = (new double[] { 2, 1, 4, 5, 3 })[new Random().Next(5)] * x;
         d.Customer.OrderDate = (new DateTime[] { new DateTime(2010, 11, 5), new DateTime(2018, 10, 3), new DateTime(1995, 9, 9), new DateTime(2012, 8, 2), new DateTime(2015, 4, 11) })[new Random().Next(5)];
-        d.Customer.ShipCountry = (new string[] { "USA", "UK", "Denmark", "Australia", "india" })[new Random().Next(5)];
+        d.Customer.ShipCountry = (new string[] { "USA", "UK", "Denmark", "Australia", "India" })[new Random().Next(5)];
         d.Freight = (new double[] { 2, 1, 4, 5, 3 })[new Random().Next(5)] * x;
         d.OrderDate = (new DateTime[] { new DateTime(2010, 11, 5), new DateTime(2018, 10, 3), new DateTime(1995, 9, 9), new DateTime(2012, 8, 2), new DateTime(2015, 4, 11) })[new Random().Next(5)];
         d.ShipCountry = (new string[] { "USA", "UK" })[new Random().Next(2)];
