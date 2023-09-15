d45cc03f31250f89494a95709ea752ad57407d90
AshokkumarKaruppasamy <107187636+AshokkumarKaruppasamy@users.noreply.github.com>
2023-09-15 09:18:35 +0530

    Update data.cs

--- ej2-asp-core-mvc/code-snippet/schedule/editor-template/custom-editor-header-footer/data.cs
+++ ej2-asp-core-mvc/code-snippet/schedule/editor-template/custom-editor-header-footer/data.cs
@@ -10,30 +10,30 @@ public List<AppointmentData> GetScheduleData()
     appData.Add(new AppointmentData
     {
         Id = 1,
-        Subject = "Meeting",
+        Subject = "Surgery - Andrew",
         StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
-        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
+        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 10, 0, 0),
         IsAllDay = false,
     },
     {
         Id: 2,
         Subject: "Consulting - John",
-        StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
-        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
+        StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 10, 0, 0),
+        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 11, 30, 0),
         IsAllDay: false
   },
   {
         Id: 3,
         Subject: "Therapy - Robert",
-        StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
-        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
+        StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 11, 30, 0),
+        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 12, 30, 0),
         IsAllDay: false
   },
   {
         Id: 4,
         Subject: "Observation - Steven",
-        StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
-        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 9, 0, 0),
+        StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 12, 30, 0),
+        EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 13, 30, 0),
         IsAllDay: false
   }
     );
