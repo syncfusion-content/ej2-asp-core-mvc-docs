ee3c2c5bb0789ae0900b315ed75a0f3870a10072
SenthilarasuBalu <119287329+SenthilarasuBalu@users.noreply.github.com>
2023-12-14 14:41:47 +0530

    removed optional

--- ej2-asp-core-mvc/code-snippet/stepper/getting-started/iconWithLabel/default.cs
+++ ej2-asp-core-mvc/code-snippet/stepper/getting-started/iconWithLabel/default.cs
@@ -4,7 +4,7 @@ public ActionResult Demo()
     iconWithLabel.Add(new Step { Label = "Cart", IconCss = "sf-icon-cart" });
     iconWithLabel.Add(new Step { Label = "Address", IconCss = "sf-icon-user" });
     iconWithLabel.Add(new Step { Label = "Delivery", IconCss = "sf-icon-transport" });
-    iconWithLabel.Add(new Step { Label = "Payment", IconCss = "sf-icon-payment", Optional = true });
+    iconWithLabel.Add(new Step { Label = "Payment", IconCss = "sf-icon-payment"});
     iconWithLabel.Add(new Step { Label = "Ordered", IconCss = "sf-icon-success" });
     ViewBag.datasource = iconWithLabel;
     
