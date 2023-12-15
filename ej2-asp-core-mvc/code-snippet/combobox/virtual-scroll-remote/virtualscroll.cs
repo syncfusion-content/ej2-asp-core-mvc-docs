360d88eb9d5bcd6b841ed5b4564057baba8362a1
mohanrajsf3477 <98141646+mohanrajsf3477@users.noreply.github.com>
2023-12-15 15:44:00 +0530

    documentation(EJ2-861557):Need to update the documentation for the virtualization in EJ2 dropdown components

---
+++ ej2-asp-core-mvc/code-snippet/combobox/virtual-scroll-remote/virtualscroll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ComboBoxController : Controller
+    {
+        public ActionResult virtualscroll()
+        {
+            ViewBag.data = new Record().RecordModelList();
+            return View();
+        }
+    }
+}
