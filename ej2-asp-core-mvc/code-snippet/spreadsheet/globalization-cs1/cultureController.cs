b03ccc2b317a1263ee0a5ce8820ff712f9514617
DinakarSF4212 <147583019+DinakarSF4212@users.noreply.github.com>
2024-10-09 15:49:14 +0530

    911245: Culture-based formatted value updated in the data source.

--- ej2-asp-core-mvc/code-snippet/spreadsheet/globalization-cs1/cultureController.cs
+++ ej2-asp-core-mvc/code-snippet/spreadsheet/globalization-cs1/cultureController.cs
@@ -6,11 +6,11 @@ public IActionResult Index()
         new { ItemName= "Sports Shoes", Date= "11.06.2014", Time= "05:56:32 AM", Quantity= 20, Price= 30, Amount= "=PRODUCT(D3;E3)", Discount= "5%", Profit= "=PRODUCT(G3;F3)" },
         new { ItemName= "Formal Shoes", Date= "27.07.2014", Time= "03:32:44 AM", Quantity= 20, Price= 15, Amount= "=PRODUCT(D4;E4)", Discount= "7,5%", Profit= "=PRODUCT(G4;F4)" },
         new { ItemName= "Sandals & Floaters", Date= "21.11.2014", Time= "06:23:54 AM", Quantity= 15, Price= "20,45", Amount= "=PRODUCT(D5;E5)", Discount= "11%", Profit= "=PRODUCT(G5;F5)" },
-        new { ItemName= "Flip- Flops & Slippers", Date= "23.06.2014", Time= "12:43:59 AM", Quantity= "30,67", Price= 10, Amount= "=PRODUCT(D6;E6)", Discount= "10%", Profit= "=PRODUCT(G6;F6)" },
+        new { ItemName= 'Flip- Flops & Slippers', Date= '23.06.2014', Time= '12:43:59 AM', Quantity= 30, Price= '10,67', Amount= '=PRODUCT(D6;E6)', Discount= '10%', Profit= '=PRODUCT(G6;F6)' },
         new { ItemName= "Sneakers", Date= "22.07.2014", Time= "10:55:53 AM", Quantity= 40, Price= 20, Amount= "=PRODUCT(D7;E7)", Discount= "13,2%", Profit= "=PRODUCT(G7;F7)" },
         new { ItemName= "Running Shoes", Date= "04.02.2014", Time= "03:44:34 AM", Quantity= 20, Price= "10,5", Amount= "=PRODUCT(D8;E8)", Discount= "3%", Profit= "=PRODUCT(G8;F8)" },
         new { ItemName= "Loafers", Date= "30.11.2014", Time= "03:12:52 AM", Quantity= 31, Price= 10, Amount= "=PRODUCT(D9;E9)", Discount= "6,67", Profit= "=PRODUCT(G9;F9)" },
-        new { ItemName= "Cricket Shoes", Date= "09.07.2014", Time= "11:32:14 AM", Quantity= 41, Price= 30, Amount= "=PRODUCT(D10;E10)", Discount= "12.5%", Profit= "=PRODUCT(G10;F10)" },
+        new { ItemName= 'Cricket Shoes', Date= '09.07.2014', Time= '11:32:14 AM', Quantity= 41, Price= 30, Amount= '=PRODUCT(D10;E10)', Discount= '12,5%', Profit= '=PRODUCT(G10;F10)' },
         new { ItemName= "T-Shirts", Date= "31.10.2014", Time= "12:01:44 AM", Quantity= 50, Price= "10,75", Amount= "=PRODUCT(D11;E11)", Discount= "9%", Profit= "=PRODUCT(G11;F11)" }
     };
     List<object> cultureList = new List<object>()
