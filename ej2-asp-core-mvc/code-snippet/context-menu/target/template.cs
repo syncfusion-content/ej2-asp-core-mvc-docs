944610db284ead7a697167e8463a65d0fd94fc20
gopi-jp <gopi.jeyachandran@syncfusion.com>
2024-12-12 16:18:20 +0530

    925858: User Guide Samples for New Image Editor Features

---
+++ ej2-asp-core-mvc/code-snippet/context-menu/target/template.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ContextMenuController : Controller
+    {
+        public IActionResult Template()
+        {
+            List<object> menuItems = new List<object>();
+            menuItems.Add(new
+            {
+                text = "Save as...  "
+            });
+            menuItems.Add(new
+            {
+                text = "View page source  "
+            });
+            menuItems.Add(new
+            {
+                text = "Inspect  "
+            });
+            ViewBag.menuItems = menuItems;
+            return View();
+        }
+    }
+}
