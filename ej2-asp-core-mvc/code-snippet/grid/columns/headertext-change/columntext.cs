3efc2fb97a4d578225e25eff05ad9ffdf23a8ec7
JamunaSundaramSF3699 <jamuna.sundaram@syncfusion.com>
2025-02-03 11:06:52 +0530

    documentation(936948-):updated

--- ej2-asp-core-mvc/code-snippet/grid/columns/headertext-change/columntext.cs
+++ ej2-asp-core-mvc/code-snippet/grid/columns/headertext-change/columntext.cs
@@ -1,3 +1,11 @@
+public IActionResult Index()
+{
+  var orders = OrdersDetails.GetAllRecords();
+  ViewBag.DataSource = orders;
+  ViewBag.dropdownData = new string[] { "OrderID", "CustomerID", "Freight", "OrderDate","ShipCountry" };
+  return View();
+}
+
 public ActionResult Index()
 {
   ViewBag.DataSource = OrdersDetails.GetAllRecords();
