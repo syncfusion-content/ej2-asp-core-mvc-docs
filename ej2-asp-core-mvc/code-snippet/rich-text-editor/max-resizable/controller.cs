57b9bc0e81f7e2da72b721adef4dcb9b8ef9ba8b
Kokila Poovendran <kokila.poovendran@syncfusion.com>
2025-02-28 00:12:51 +0530

    897361: ASP.NET CORE Documentation Cleanup 2

--- ej2-asp-core-mvc/code-snippet/rich-text-editor/max-resizable/controller.cs
+++ ej2-asp-core-mvc/code-snippet/rich-text-editor/max-resizable/controller.cs
@@ -3,6 +3,7 @@
 
         public ActionResult Index()
         {
+            ViewBag.value = @"<p>The Rich Text Editor component is WYSIWYG ('what you see is what you get') editor that provides the best user experience to create and update the content. Users can format their content using standard toolbar commands.</p>";
             return View();
         }
 
