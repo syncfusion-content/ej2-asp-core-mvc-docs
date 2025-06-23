3904ba28364c4d57263cb9b50cc03bdea50d1d07
Vinoth2562000 <104135878+Vinoth2562000@users.noreply.github.com>
2025-06-23 13:29:13 +0530

    965200: Implement Feature Documentation for Code Block in Rich Text Editor

--- ej2-asp-core-mvc/code-snippet/rich-text-editor/code-block/controller.cs
+++ ej2-asp-core-mvc/code-snippet/rich-text-editor/code-block/controller.cs
@@ -3,12 +3,13 @@ public class HomeController : Controller
 
     public ActionResult Index()
     {
-        ViewBag.items = new[] { "Undo", "Redo", "|", "CodeBlock", "|", "Bold", "Italic", "Underline", "StrikeThrough", "InlineCode", "|", "CreateLink", "Image", "CreateTable", "Blockquote", "|", "BulletFormatList", "NumberFormatList", "|", "Formats", "Alignments", "|", "Outdent", "Indent", "|",
+            ViewBag.items = new[] { "Undo", "Redo", "|", "CodeBlock", "|", "Bold", "Italic", "Underline", "StrikeThrough", "InlineCode", "|", "CreateLink", "Image", "CreateTable", "Blockquote", "|", "BulletFormatList", "NumberFormatList", "|", "Formats", "Alignments", "|", "Outdent", "Indent", "|",
                         "FontColor", "BackgroundColor", "FontName", "FontSize", "|", "LowerCase", "UpperCase", "|", "EmojiPicker", "|", "SourceCode" };
-        ViewBag.languages = new[] { {label: "HTML", language: "html"},
-                    {label: "JavaScript", language: "javascript"},
-                    {label: "CSS", language: "css"},
-                    {label: "Plain Text", language: "plaintext"}};
+            ViewBag.languages = new[] { 
+                new {label= "HTML", language= "html"},
+                new {label= "JavaScript", language= "javascript"},
+                new {label= "CSS", language= "css"},
+                new {label= "Plain Text", language= "plaintext"}};
         return View();
     }
 }
