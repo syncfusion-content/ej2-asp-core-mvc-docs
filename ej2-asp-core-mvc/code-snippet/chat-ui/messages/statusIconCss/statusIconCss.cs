cfade70f8a5145a9d1a2e4d5fa33ad7aa3c1a52f
premanandh2205 <premanandh.v@syncfusion.com>
2025-06-24 11:02:22 +0530

    965486: Updated the correction in chat ui and Ai Assistview

--- ej2-asp-core-mvc/code-snippet/chat-ui/messages/statusIconCss/statusIconCss.cs
+++ ej2-asp-core-mvc/code-snippet/chat-ui/messages/statusIconCss/statusIconCss.cs
@@ -2,26 +2,33 @@ using Syncfusion.EJ2.InteractiveChat;
 
 public ChatUIUser CurrentUser { get; set; }
 public List<ChatUIMessage> ChatMessagesData { get; set; } = new List<ChatUIMessage>();
-public ChatUIUser CurrentUserModel { get; set; } = new ChatUIUser() { Id = "user1", User = "Albert", StatusIconCss = "e-icons e-user-online" };
-public ChatUIUser MichaleUserModel { get; set; } = new ChatUIUser() { Id = "user2", User = "Michale Suyama", StatusIconCss = "e-icons e-user-away" };
+public ChatUIUser CurrentUserModel { get; set; } = new ChatUIUser() { Id = "user1", User = "Alice Brown", StatusIconCss = "e-icons e-user-online" };
+public ChatUIUser CommunityMessageUser1 { get; set; } = new ChatUIUser() { Id = "user2", User = "Michale Suyama", StatusIconCss = "e-icons e-user-away" };
+public ChatUIUser CommunityMessageUser2 { get; set; } = new ChatUIUser() { Id = "user3", User = "Charlie", StatusIconCss = "e-icons e-user-busy" };
+public ChatUIUser CommunityMessageUser3 { get; set; } = new ChatUIUser() { Id = "user4", User = "Jordan Peele", StatusIconCss = "e-icons e-user-offline" };
 
 public ActionResult StatusIconCss()
 {
     CurrentUser = CurrentUserModel;
     ChatMessagesData.Add(new ChatUIMessage()
     {
-        Text = "Hi Michale, are we on track for the deadline?",
+        Text = "Hey Michale, Charlie! Seen the latest posts in the Design Community? Amazing projects!",
         Author = CurrentUserModel
     });
     ChatMessagesData.Add(new ChatUIMessage()
     {
-        Text = "Yes, the design phase is complete.",
-        Author = MichaleUserModel
+        Text = "Hi Alice! Yes, Dana’s new UI design is incredible.",
+        Author = CommunityMessageUser1
     });
     ChatMessagesData.Add(new ChatUIMessage()
     {
-        Text = "I’ll review it and send feedback by today.",
-        Author = CurrentUserModel
+        Text = "Dana’s work is so inspiring!",
+        Author = CommunityMessageUser3
+    });
+    ChatMessagesData.Add(new ChatUIMessage()
+    {
+        Text = "I am excited to see the new projects.",
+        Author = CommunityMessageUser2
     });
     ViewBag.ChatMessagesData = ChatMessagesData;
     ViewBag.CurrentUser = CurrentUser;
