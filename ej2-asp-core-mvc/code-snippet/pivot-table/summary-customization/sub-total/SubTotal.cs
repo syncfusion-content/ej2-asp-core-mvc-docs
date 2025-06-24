7fc9d14d0ab53346de0bd374766d70295b80dbf3
AbirhamSF4501 <163090353+AbirhamSF4501@users.noreply.github.com>
2025-06-24 15:39:38 +0530

    958641: Resolved the MVC sample issue

--- ej2-asp-core-mvc/code-snippet/pivot-table/summary-customization/sub-total/SubTotal.cs
+++ ej2-asp-core-mvc/code-snippet/pivot-table/summary-customization/sub-total/SubTotal.cs
@@ -2,5 +2,6 @@ public ActionResult Index()
 {
     var data = GetPivotData();
     ViewBag.DataSource = data;
+    ViewBag.drilledMembers = new string[] { "France", "Germany" };
     return View();
 }
