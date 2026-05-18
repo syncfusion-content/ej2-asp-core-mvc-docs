75de532a07144aaa7d608ebe8b79795e4c03a2f3
YukeshNarayanB-SF5087 <yukeshnarayan.b@syncfusion.com>
2026-05-18 23:47:52 +0530

    Refactor OrdersDetails class and add HomeController

--- ej2-asp-core-mvc/code-snippet/grid/getting-start-core/gridcolumns/orderDetails.cs
+++ ej2-asp-core-mvc/code-snippet/grid/getting-start-core/gridcolumns/orderDetails.cs
@@ -1,34 +1,44 @@
-public class OrdersDetails
-{
-    public static List<OrdersDetails> order = new List<OrdersDetails>();
-    public OrdersDetails()
+   public class HomeController : Controller
     {
-
-    }
-    public OrdersDetails(int OrderID, string CustomerId, DateTime OrderDate, string ShipCountry, double Freight)
-    {
-        this.OrderID = OrderID;
-        this.CustomerID = CustomerId;
-        this.OrderDate = OrderDate;
-        this.ShipCountry = ShipCountry;
-        this.Freight = Freight;
-    }
-    public static List<OrdersDetails> GetAllRecords()
-    {
-        if (order.Count() == 0)
+        public IActionResult Index()
         {
-            order.Add(new OrdersDetails(10248, "Ana Trujillo", new DateTime(2025, 1, 12), "France", 32.38));
-            order.Add(new OrdersDetails(10249, "Martin Sommer", new DateTime(2025, 1, 15), "Germany", 11.61));
-            order.Add(new OrdersDetails(10250, "Thomas Hardy", new DateTime(2025, 2, 5), "Brazil", 65.83));
-            order.Add(new OrdersDetails(10251, "Elizabeth Lincoln", new DateTime(2025, 2, 18), "France", 41.34));
-            order.Add(new OrdersDetails(10252, "Victoria Ashworth", new DateTime(2025, 3, 10), "Belgium", 51.30));
-            order.Add(new OrdersDetails(10253, "Martine Rance", new DateTime(2025, 3, 22), "Brazil", 58.17));    
+            var Order = OrdersDetails.GetAllRecords();
+            ViewBag.DataSource = Order;
+            return View();
         }
-        return order;
+ 
+        public class OrdersDetails
+        {
+            public static List<OrdersDetails> order = new List<OrdersDetails>();
+            public OrdersDetails()
+            {
+ 
+            }
+            public OrdersDetails(int OrderID, string CustomerId, DateTime OrderDate, string ShipCountry, double Freight)
+            {
+                this.OrderID = OrderID;
+                this.CustomerID = CustomerId;
+                this.OrderDate = OrderDate;
+                this.ShipCountry = ShipCountry;
+                this.Freight = Freight;
+            }
+            public static List<OrdersDetails> GetAllRecords()
+            {
+                if (order.Count() == 0)
+                {
+                    order.Add(new OrdersDetails(10248, "Ana Trujillo", new DateTime(2025, 1, 12), "France", 32.38));
+                    order.Add(new OrdersDetails(10249, "Martin Sommer", new DateTime(2025, 1, 15), "Germany", 11.61));
+                    order.Add(new OrdersDetails(10250, "Thomas Hardy", new DateTime(2025, 2, 5), "Brazil", 65.83));
+                    order.Add(new OrdersDetails(10251, "Elizabeth Lincoln", new DateTime(2025, 2, 18), "France", 41.34));
+                    order.Add(new OrdersDetails(10252, "Victoria Ashworth", new DateTime(2025, 3, 10), "Belgium", 51.30));
+                    order.Add(new OrdersDetails(10253, "Martine Rance", new DateTime(2025, 3, 22), "Brazil", 58.17));
+                }
+                return order;
+            }
+            public int? OrderID { get; set; }
+            public string CustomerID { get; set; }
+            public DateTime OrderDate { get; set; }
+            public string ShipCountry { get; set; }
+            public double Freight { get; set; }
+        }
     }
-    public int? OrderID { get; set; }
-    public string CustomerID { get; set; }
-    public DateTime OrderDate { get; set; }
-    public string ShipCountry { get; set; }
-    public double Freight { get; set; }
-}
