df5a8179a6f9d3811a855d999382274ac973ce5e
Faith1399 <angelinfaithsheeba.p@syncfusion.com>
2026-05-28 16:04:06 +0530

    1027860: Updated Core and MVC getting started content

--- ej2-asp-core-mvc/code-snippet/pivot-table/getting-start-core/pivot-table/pivottable.cs
+++ ej2-asp-core-mvc/code-snippet/pivot-table/getting-start-core/pivot-table/pivottable.cs
@@ -1,12 +1,29 @@
-public class HomeController : Controller
-{
-    public IActionResult Index()
-    {
-        var data = PivotDataSource.GetPivotData();
-        ViewBag.DataSource = data;
-        return View();
-    }
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace PivotTable.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<PivotData> PivotDataSource { get; set; }
+        public void OnGet()
+        {
+            PivotDataSource = GetDefaultData();
+        }
+
+        public List<PivotData> GetDefaultData()
+        {
+            List<PivotData> DefaultData = new List<PivotData>()
+            {
+                new PivotData { Sold = 31, Amount = 52824, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q1" },
+                new PivotData { Sold = 51, Amount = 86904, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q2" },
+                new PivotData { Sold = 90, Amount = 153360, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q3" },
+                new PivotData { Sold = 25, Amount = 42600, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q4" }
+            };
+            return DefaultData;
+            
+        }
+    }
     public class PivotData
     {
         public int Sold { get; set; }
@@ -15,19 +32,5 @@ public class HomeController : Controller
         public string Products { get; set; }
         public string Year { get; set; }
         public string Quarter { get; set; }
-    }
-
-    public static class PivotDataSource
-    {
-        public static List<PivotData> GetPivotData()
-        {
-            return new List<PivotData>
-                {
-                    new PivotData { Sold = 31, Amount = 52824, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q1" },
-                    new PivotData { Sold = 51, Amount = 86904, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q2" },
-                    new PivotData { Sold = 90, Amount = 153360, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q3" },
-                    new PivotData { Sold = 25, Amount = 42600, Country = "France", Products = "Mountain Bikes", Year = "FY 2025", Quarter = "Q4" }
-                };
-        }
     }
 }
